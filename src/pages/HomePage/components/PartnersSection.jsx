39acc121ea5a297ee1e7895da9eb2ea0d4a1500c
Benjamin Mikiten <benjaminmikiten@gmail.com>
2020-11-12 12:48:11 -0600

    Add progress bar animation

--- src/pages/HomePage/components/PartnersSection.jsx
+++ src/pages/HomePage/components/PartnersSection.jsx
@@ -34,9 +34,62 @@ const PartnerItem = () => {
     </StyledPartnerItem>
   );
 };
+
 const PartnersSectionStyles = styled.section`
   padding: 120px 0;
+  position: relative;
+  &:after,
+  &:before {
+    content: "";
+    display: block;
+    position: absolute;
+    bottom: 0;
+  }
 `;
+
+const SectionTitle = styled.div`
+  ${({ theme }) => theme.grid.LayoutContainer};
+  h2 {
+    ${({ theme }) => theme.textStyles.largeTitle};
+    padding-bottom: 1rem;
+  }
+`;
+
+const StyledProgressBar = styled.div`
+  width: 100%;
+  position: relative;
+  height: 8px;
+  padding-top: 4rem;
+  border: solid 1px blue;
+`;
+
+const Bar = styled(motion.div)`
+  position: absolute;
+  height: 4px;
+  top: 50%;
+  transform: translateY(-50%);
+  background-color: ${({ theme }) => theme.colors.orange};
+`;
+const Dot = styled(motion.div)`
+  height: 8px;
+  width: 8px;
+  border-radius: 100%;
+  background-color: ${({ theme }) => theme.colors.carrot};
+  position: absolute;
+  top: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const ProgressBar = ({ progress }) => {
+  const dotAnimation = { left: transform(progress, [0, 1], ["0%", "80%"]) };
+  const barAnimation = { width: transform(progress, [0, 1], ["0%", "80%"]) };
+  return (
+    <StyledProgressBar>
+      <Bar animate={barAnimation} />
+      <Dot animate={dotAnimation} />
+    </StyledProgressBar>
+  );
+};
 
 const PartnersSection = () => {
   return (
@@ -46,7 +99,10 @@ const PartnersSection = () => {
           const itemsAnimation = { left: transform(progress, [0, 1], ["25%", "-25%"]) };
           console.log("partners prog", progress);
           return (
-            <PartnersSectionStyles>
+            <PartnersSectionStyles progress={progress}>
+              <SectionTitle>
+                <h2>Trusted by:</h2>
+              </SectionTitle>
               <PartnerItemsWrapper>
                 <PartnerItems animate={itemsAnimation}>
                   <PartnerItem />
@@ -56,6 +112,7 @@ const PartnersSection = () => {
                   <PartnerItem />
                 </PartnerItems>
               </PartnerItemsWrapper>
+              <ProgressBar progress={progress} />
             </PartnersSectionStyles>
           );
         }}
