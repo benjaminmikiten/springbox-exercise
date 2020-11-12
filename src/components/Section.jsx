518cc6d68e9b24474653afa7c6c86674a72ce72c
Benjamin Mikiten <benjaminmikiten@gmail.com>
2020-11-12 13:43:13 -0600

    add placeholder sections

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import styled from "styled-components";
 
-const SectionStyle = styled.section``;
+const SectionStyle = styled.section`
+  min-height: 1200px;
+  height: 100vh;
+`;
 
-// export const SectionContext = React.createContext({});
+// THIS IS JUST TO ADD HEIGHT TO THE PAGE FOR A REALISTIC IDEA OF SCROLL EFFECTS
 
 const Section = ({ children }) => {
   return <SectionStyle>{children}</SectionStyle>;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import HomeHero from "./components/HomeHero";
 import PartnersSection from "./components/PartnersSection";
 import OurSolutionsSection from "./components/OurSolutionsSection";
+import Section from "../../components/Section";
 
 const StyledHomePage = styled.div``;
 
@@ -13,9 +14,13 @@ const HomePage = () => {
       <PartnersSection />
       <OurSolutionsSection />
       {/* <CovidSolutionsSection /> */}
+      <Section>COVID SECTION</Section>
       {/* GetInTouchSection */}
+      <Section>GET IN TOUCH SECTION</Section>
       {/* PlatformSection */}
+      <Section>PLATFORM SECTION</Section>
       {/* TrendingSection */}
+      <Section>TRENDING SECTION</Section>
     </StyledHomePage>
   );
 };
